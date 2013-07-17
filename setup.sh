f0aab0fb8468ea01beea802ba671024d06c4f454
TudorCampean <tudor.campean@webappsproduction.com>
2013-07-17 17:54:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,13 +29,10 @@ cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
+
 git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+
 
