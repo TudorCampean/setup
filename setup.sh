7f0cc831181c55fc56b527363ed4716741f2e350
TudorCampean <tudor.campean@webappsproduction.com>
2013-07-17 17:45:00 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10.13
+nvm use v0.10.13
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -19,12 +19,6 @@ npm install -g jshint
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
-
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
